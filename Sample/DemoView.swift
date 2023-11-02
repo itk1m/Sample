cd068ebd62261c17e26d7de778e878ac03f0154f
itk1m <isaactaekim987@gmail.com>
2023-11-02 14:05:43 -0400

    Send new view

---
+++ Sample/DemoView.swift
@@ -0,0 +1,18 @@
+//
+//  DemoView.swift
+//  Sample
+//
+//  Created by Isaac Kim on 11/2/23.
+//
+
+import SwiftUI
+
+struct DemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DemoView()
+}
